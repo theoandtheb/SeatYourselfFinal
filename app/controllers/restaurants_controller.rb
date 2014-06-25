a41814fa5299bb7629e0afaab2443272ab323933
Arjun <arjun.bali96@gmail.com>
2014-06-25 17:53:46 -0400

    finished views and added basic validations

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -22,9 +22,17 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
+    @restaurant = Restaurant.find(parmas[:id])
+
+    if @restaurant.update_attribute(restaurant_params)
+      redirect_to restaurant_path(@restaurant), :notice => "Updated!"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625191844) do
+ActiveRecord::Schema.define(version: 20140625215019) do
 
   create_table "reservations", force: true do |t|
     t.time     "time"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20140625191844) do
     t.integer  "max_occupancy"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "description"
+    t.integer  "phone_number"
   end
 
   create_table "users", force: true do |t|
